aa5fbe0eb460839955fc0a2ddc9c14bbc3688145
krogank9 <wifi.mouse.app@gmail.com>
2019-07-07 23:22:01 -0400

    fix scrolling

--- handle-modals.js
+++ handle-modals.js
@@ -1,11 +1,11 @@
 /* open a modal when a "read more" link is clicked in portfolio section */
 $(".portfolio-read-more").click(function(e){
-	var modalNum = $(e.target).attr("data-modal-num");
-	$(".portfolio-modal-container.modal-"+modalNum).css("display", "block").scrollTop(0);
+	let modalNum = $(e.target).attr("data-modal-num");
+	let modal = $(".portfolio-modal-container.modal-"+modalNum);
+	modal.css("display", "block").scrollTop(0);
 	
 	/* disable scrolling on body to allow scrolling of modal */
-	$("body").css("overflow", "hidden");
-	$('body').css('position','fixed');
+	bodyScrollLock.disableBodyScroll(modal[0]);
 });
 
 /* close (hide) all modals upon clicking a close button within one */
@@ -13,6 +13,5 @@ $(".portfolio-close-button, .portfolio-x-button").click(function() {
 	$(".portfolio-modal-container").css("display", "none");
 	
 	/* re enable scrolling on body now that modal has disappeared */
-	$("body").css("overflow", "auto");
-	$('body').css('position','unset');
+	bodyScrollLock.clearAllBodyScrollLocks();
 });
